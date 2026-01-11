3fa5758424ba3cda36817b11132722bf780017df
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2026-01-11 17:38:28 -0600

    Initial commit

---
+++ src/main/java/com/statuswatchr/statuswatchr/StatusWatchrApplication.java
@@ -0,0 +1,13 @@
+package com.statuswatchr.statuswatchr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatusWatchrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatusWatchrApplication.class, args);
+	}
+
+}
