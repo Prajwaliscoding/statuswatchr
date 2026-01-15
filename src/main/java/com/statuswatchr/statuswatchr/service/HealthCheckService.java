b1429cabd51b3dc795b86d9d1ee56445374bb879
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2026-01-14 20:41:14 -0600

    Added the monitoring part of the project that is HealthCheckService.java and all its content

--- src/main/java/com/statuswatchr/statuswatchr/service/HealthCheckService.java
+++ src/main/java/com/statuswatchr/statuswatchr/service/HealthCheckService.java
@@ -1,8 +1,33 @@
 package com.statuswatchr.statuswatchr.service;
 
+import com.statuswatchr.statuswatchr.model.Status;
 import org.springframework.stereotype.Service;
+
+import java.net.HttpURLConnection;
+import java.net.URI;
 
 @Service
 public class HealthCheckService {
-    
+
+    public record CheckResult(Status status, String error, Integer httpCode){}
+
+    public CheckResult check(String url){
+        try{
+            HttpURLConnection con = (HttpURLConnection) URI.create(url).toURL().openConnection();
+
+            con.setRequestMethod("GET");
+            con.setConnectTimeout(3000);
+            con.setReadTimeout(3000);
+
+            int code = con.getResponseCode();
+
+            if(code>=200 && code <300){
+                return new CheckResult(Status.UP, null, code);
+            }
+            return new CheckResult(Status.DOWN, "HTTP " + code, code);
+        }
+        catch (Exception e) {
+            return new CheckResult(Status.DOWN, e.getClass().getSimpleName() + ":" + e.getMessage(),null);
+        }
+    }
 }
