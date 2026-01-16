df2e6eecae79ea1af0525772dcf81ffed0173680
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2026-01-16 01:12:26 -0600

    Made the basic monitoring of services work with statuses UP or Down with lastChecked and lastError

--- src/main/java/com/statuswatchr/statuswatchr/service/WatchrScheduler.java
+++ src/main/java/com/statuswatchr/statuswatchr/service/WatchrScheduler.java
@@ -1,9 +1,12 @@
 package com.statuswatchr.statuswatchr.service;
-
-
+import com.statuswatchr.statuswatchr.model.Watchr;
 import com.statuswatchr.statuswatchr.repository.WatchrRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import java.time.Instant;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -12,5 +15,27 @@ public class WatchrScheduler {
     private final HealthCheckService checker;
     private final WatchrRepository repo;
 
-    
+    @Scheduled(fixedDelay = 5000)
+    @Transactional
+    public void runChecks(){
+        List<Watchr> watchrs = repo.findAll();
+        Instant now = Instant.now();
+
+        for(Watchr w : watchrs) {
+            if(!isDue(w,now)) continue;
+            var result = checker.check(w.getUrl());
+
+            w.setStatus(result.status());
+            w.setLastCheckedAt(now);
+            w.setLastError(result.error());
+        }
+    }
+
+    private boolean isDue(Watchr w, Instant now){
+        if(w.getLastCheckedAt() == null ) return true;
+
+        long timeGoneBy = now.getEpochSecond() - w.getLastCheckedAt().getEpochSecond();
+        return timeGoneBy >= w.getIntervalSecond();
+    }
+
 }
