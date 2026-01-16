78f206937dd6da36b1b4d6b9efed4516605802aa
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2026-01-15 21:31:41 -0600

    Added the schedulerbusiness logic in the service layer for calling the HealthCheckService to check the url

--- src/main/java/com/statuswatchr/statuswatchr/service/WatchrScheduler.java
+++ src/main/java/com/statuswatchr/statuswatchr/service/WatchrScheduler.java
@@ -1,4 +1,16 @@
 package com.statuswatchr.statuswatchr.service;
 
+
+import com.statuswatchr.statuswatchr.repository.WatchrRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class WatchrScheduler {
+
+    private final HealthCheckService checker;
+    private final WatchrRepository repo;
+
+    
 }
