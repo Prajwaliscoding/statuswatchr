c1d3127a3b7fd645d137a70d91b7639f618a0f98
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2026-01-17 20:21:38 -0600

    Added the Incident as the model

--- src/main/java/com/statuswatchr/statuswatchr/model/Incident.java
+++ src/main/java/com/statuswatchr/statuswatchr/model/Incident.java
@@ -1,7 +1,29 @@
 package com.statuswatchr.statuswatchr.model;
-
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import java.time.Instant;
 
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "incidents")
 public class Incident {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    private Watchr watchr;
+
+    @Column(nullable = false)
+    private Instant startedAt;
+    private Instant resolvedAt;
+    private String errorMessage;
+
+    public boolean isOpen(){
+        return resolvedAt == null;
+    }
 }
