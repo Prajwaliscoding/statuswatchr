997fb4b44840d9d1fce0c5da55ff33ef5ebe0cae
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2026-01-19 23:49:19 -0600

    Updated IncidentRepository for logging feature

--- src/main/java/com/statuswatchr/statuswatchr/repository/IncidentRepository.java
+++ src/main/java/com/statuswatchr/statuswatchr/repository/IncidentRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface IncidentRepository extends JpaRepository<Incident, Long> {
-    Optional<Incident> findFirstByWatchrAndResolvedAtIsNull(Watchr watchr);  
+    Optional<Incident> findFirstByWatchrAndResolvedAtIsNull(Watchr watchr);
 }
