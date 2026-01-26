208e248f376f8b8cada1b505f1f8fec7312d226f
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2026-01-25 23:53:00 -0600

    Removed unnecessary comments and imports

--- src/main/java/com/statuswatchr/statuswatchr/service/IncidentService.java
+++ src/main/java/com/statuswatchr/statuswatchr/service/IncidentService.java
@@ -1,7 +1,6 @@
 package com.statuswatchr.statuswatchr.service;
 
 import com.statuswatchr.statuswatchr.dto.IncidentResponse;
-import com.statuswatchr.statuswatchr.dto.WatchrResponse;
 import com.statuswatchr.statuswatchr.model.Incident;
 import com.statuswatchr.statuswatchr.repository.IncidentRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,16 +18,16 @@ public class IncidentService {
     public IncidentResponse toResponse(Incident i){
         return new IncidentResponse(
                 i.getId(),
-                i.getWatchr().getId(),  // Incident → has a Watchr object
-                i.getStartedAt(),       // Watchr → has an id. basically, relation wala ko directly paidaina
+                i.getWatchr().getId(),
+                i.getStartedAt(),
                 i.getResolvedAt(),
                 i.getErrorMessage()
         );
     }
 
     public List<IncidentResponse> getAll() {
-        return incidentRepo.findAll().stream() // findAll, get one by one with stream
-                .map(this::toResponse)         // then, map that Incident type to a IncidentResponse type with
-                .toList();                     // the help of toResponse() and convert all to a list with toList()
+        return incidentRepo.findAll().stream()
+                .map(this::toResponse)
+                .toList();
     }
 }
