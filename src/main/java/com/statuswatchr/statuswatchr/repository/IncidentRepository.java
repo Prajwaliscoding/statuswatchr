4f8e111503c464b500564d532507d464297f6296
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2026-01-26 23:17:14 -0600

    Started adding the api for incident history and pagination

--- src/main/java/com/statuswatchr/statuswatchr/repository/IncidentRepository.java
+++ src/main/java/com/statuswatchr/statuswatchr/repository/IncidentRepository.java
@@ -2,10 +2,15 @@ package com.statuswatchr.statuswatchr.repository;
 
 import com.statuswatchr.statuswatchr.model.Incident;
 import com.statuswatchr.statuswatchr.model.Watchr;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface IncidentRepository extends JpaRepository<Incident, Long> {
     Optional<Incident> findFirstByWatchrAndResolvedAtIsNull(Watchr watchr);
+
+    Page<Incident> findByResolvedAtIsNull(Pageable pageable);
+
 }
