565237311d9b312493c4fbc50a895629b5f179c1
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2026-01-27 19:16:44 -0600

    Added the business logic for getting the incidents history in the service layer

--- src/main/java/com/statuswatchr/statuswatchr/service/IncidentService.java
+++ src/main/java/com/statuswatchr/statuswatchr/service/IncidentService.java
@@ -2,8 +2,12 @@ package com.statuswatchr.statuswatchr.service;
 
 import com.statuswatchr.statuswatchr.dto.IncidentResponse;
 import com.statuswatchr.statuswatchr.model.Incident;
+import com.statuswatchr.statuswatchr.model.Watchr;
 import com.statuswatchr.statuswatchr.repository.IncidentRepository;
+import com.statuswatchr.statuswatchr.repository.WatchrRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,9 +15,8 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class IncidentService {
-
     private final IncidentRepository incidentRepo;
-
+    private final WatchrRepository watchrRepo;
 
     public IncidentResponse toResponse(Incident i){
         return new IncidentResponse(
@@ -24,10 +27,26 @@ public class IncidentService {
                 i.getErrorMessage()
         );
     }
-
     public List<IncidentResponse> getAll() {
         return incidentRepo.findAll().stream()
                 .map(this::toResponse)
                 .toList();
+    }
+
+    public Page<IncidentResponse> getOpen(Pageable pageable) {
+        return incidentRepo.findByResolvedAtIsNull(pageable).map(this::toResponse);
+        // findByResolvedAtIsNull() method is made in IncidentRepository. This query written in method form.
+    }
+    public Page<IncidentResponse> getAllPage(Pageable pageable) {
+        return incidentRepo.findAll(pageable).map(this::toResponse);
+    }
+    public Page<IncidentResponse> getByWatchr(Long id, boolean open, Pageable pageable) {
+        Watchr w = watchrRepo.findById(id)
+                .orElseThrow(()->new RuntimeException("Watchr not found: "+id));
+
+        if(open){     // if (open == true)
+            return incidentRepo.findByWatchrAndResolvedAtIsNull(w, pageable).map(this::toResponse);
+        }
+        return incidentRepo.findByWatchr(w,pageable).map(this::toResponse);
     }
 }
